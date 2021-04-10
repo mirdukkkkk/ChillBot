c162a056808e826cd4002cabe178354f5c91b03d
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-04-10 23:56:29 +0300

    update user.js

--- database/user.js
+++ database/user.js
@@ -1,1 +1,8 @@
-
+const settings = require("./settings.json");
+const schema = Mongo.Schema({
+  userID: String,
+  badges: { type: Array, default: [] },
+  embcolor: { type: String, default: settings.embcolor },
+  messages: { type: Number, default: 0 }
+});
+module.exports = Mongo.model("User", schema);
