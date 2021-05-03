e16a4e4775063f8bd1eef43bee21b2a10e4dd3b0
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-03 19:42:55 +0300

    Ласт большой коммит за сегодня

---
+++ structures/ChillBotCommand.js
@@ -0,0 +1,13 @@
+class ChillBotCommand {
+    constructor(name, options = {}) {
+        this.name = name;
+        this.description = options.description || 'Описание для данной комнды не установлено.';
+        this.category = options;
+        this.usage = options.usage || 'Пример использования команды не установлен.';
+        this.args = options.args || false;
+        this.onlyDev = options.onlyDev || false;
+        this.aliases = options.aliases || [];
+        this.cooldown = options.cooldown || 0;
+        this.premium = options.premium || false;
+    }
+}
