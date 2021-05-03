121217133d9bec72908b12e86941b81354d31c9f
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-03 19:42:55 +0300

    Ласт большой коммит за сегодня

--- structures/ChillBotClient.js
+++ structures/ChillBotClient.js
@@ -1,1 +1,32 @@
-const { MessageClient, Collection } = require()
+const { Client, Collection } = require('discord.js');
+const { MongoClient } = require('mongodb');
+const LoaderMaster = require('../masters/LoaderMaster');
+
+class ChillBotClient extends Client {
+    constructor(options) {
+        super(options);
+        this.settings = require('../settings.json');
+
+        this.mongo = new MongoClient(this.settings.database, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        this.listeners = new Collection();
+        this.commands = new Collection();
+        this.interactions = new Collection();
+    }
+
+    get database() {
+        return this.mongo.db('Main');
+    }
+
+    async _start() {
+        await this.mongo.connect();
+        await LoaderMaster.loadListeners('../listeners');
+        await LoaderMaster.loadCommands('../commands');
+        await LoaderMaster.loadInteractions('../interactions');
+        return this.login(this.settings.token).catch(console.error);
+    }
+};
+module.exports = ChillBotClient;
