a03a0afed0bb230e0e9724417abeabe13bed2872
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-03 18:01:37 +0300

    Delete database directory

--- database/user.js
+++
@@ -1,8 +0,0 @@
-const settings = require("./settings.json");
-const schema = Mongo.Schema({
-  userID: String,
-  badges: { type: Array, default: [] },
-  embcolor: { type: String, default: settings.embcolor },
-  messages: { type: Number, default: 0 }
-});
-module.exports = Mongo.model("User", schema);
