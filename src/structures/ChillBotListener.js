4ae33f4f10dca4233f694505a6f1fbee6a817e8e
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-18 18:39:30 +0300

    Все в src

---
+++ src/structures/ChillBotListener.js
@@ -0,0 +1,8 @@
+class ChillBotListener {
+   constructor(name, options = {}) {
+       this.name = name;
+       this.ws = options.ws || false;
+       this.event = options.event || null;
+   }
+}
+module.exports = ChillBotListener;
