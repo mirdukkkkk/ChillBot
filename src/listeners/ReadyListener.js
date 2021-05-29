ccd52d1fad274f919264767efe6f825b55241a4b
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-29 23:09:06 +0300

    Обновил эвент запуска

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const ChillBotListener = require('../structures/ChillBotListener');
 
 class ReadyListener extends ChillBotListener {
@@ -7,6 +8,15 @@ class ReadyListener extends ChillBotListener {
 
     async run(client) {
         client.user.setActivity(`на сервер Just Chilling`, { type: 3 });
+        setInterval(() => {
+            client.channels.cache.get(client.settings.panel.channel).messages.cache.get(client.settings.panel.message).edit(
+                new MessageEmbed()
+                .setTitle('🔧 | Статистика бота')
+                .setColor(client.settings.colors.main)
+                .setFooter('ChillBot', this.client.user.displayAvatarURL({ format: 'webp', size: 2048 }))
+                .setTimestamp()
+            );
+        }, client.settings.panel.interval);
         console.log('[+] ChillBot был запущен!');
     }
 }
