c6cb3657e4b7a859f8028dcdec888a5133859287
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-29 17:09:22 +0300

    Убрал интеракции

--- src/structures/ChillBotInteraction.js
+++
@@ -1,7 +0,0 @@
-class ChillBotInteraction {
-    constructor(name, options = {}) {
-        this.name = name;
-        this.active = options.active || true;
-    }
-}
-module.exports = ChillBotInteraction
