dbb497e438de28d92efbbaeb6c0d8d25ffbae451
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-29 18:57:35 +0300

    Структура для ДЖСа

---
+++ src/structures/DJS/ChillBotMessage.js
@@ -0,0 +1,29 @@
+const { Message, APIMessage, MessageEmbed } = require('discord.js');
+
+class ChillBotMessage extends Message {
+  reply(...args) {
+    const apiMessage = APIMessage.create(this.channel, ...args).resolveData();
+    apiMessage.data.message_reference = {
+      message_id: this.id,
+      guild_id: this.guild?.id
+    };
+    apiMessage.data.allowed_mentions = {
+      ...(apiMessage.data.allowed_mentions || {}),
+      replied_user: apiMessage.options.ping ?? false
+    };
+
+    return this.channel.send(apiMessage);
+  }
+  fail(...args) {
+    return this.reply(
+        new MessageEmbed()
+        .setTitle(`${this.client.settings.emojis.failure} | Ошибка`)
+        .setColor('ff3333')
+        .setDescription(args)
+        .setFooter('ChillBot', this.client.user.displayAvatarURL({ format: 'webp', size: 2048 }))
+        .setTimestamp()
+    );
+  }
+}
+
+module.exports = ChillBotMessage;
