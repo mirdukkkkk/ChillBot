7811a51dcb7fd7bf8c0e88e12d729f825879e187
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-29 21:00:11 +0300

    Добавил одну крутую штуку

---
+++ src/listeners/message/MessageUpdateListener.js
@@ -0,0 +1,18 @@
+const ChillBotListener = require('../../structures/ChillBotListener');
+const CommandsExecutorMaster = require('../../masters/CommandsExecutorMaster');
+const MessageListener = require('./MessageListener');
+
+class MessageUpdateListener extends ChillBotListener {
+    constructor() {
+        super('MessageUpdateListener', { event: 'messageUpdate' });
+    }
+
+    async run(client, oldMessage, newMessage) {
+        if(!newMessage.guild || oldMessage.content === newMessage.content) return;
+
+        const executor = new CommandsExecutorMaster(newMessage, client);
+        executor.runCommand();
+    }
+}
+
+module.exports = MessageListener;
