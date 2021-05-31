03f5cf354e66a732707b965225ae6a71e8554925
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-31 16:44:24 +0300

    Сделал ахуенный хелп

--- src/commands/Main/HelpCommand.js
+++ src/commands/Main/HelpCommand.js
@@ -31,7 +31,30 @@ class HelpCommand extends ChillBotCommand {
             const embed = new MessageEmbed()
             .setTitle(`${message.client.settings.emojis.info} | Помощь по командам`)
             .setColor(message.client.settings.colors.main)
-            .setDescription()
+            .setDescription(`Узнать и нформация о определённом модуле можно через \`${data.prefix}\``)
+            .setFooter(message.guild.name, message.guild.iconURL())
+            .setTimestamp();
+
+            for (const category of categories) {
+                embed.addField(category.name, category.description, true);
+            }
+
+            message.reply(embed);
+        } if(args[0]) {
+            const category = categories.find((c) => c.name === args[0] || c.key === args[0]);
+            if(!category) return message.fail(`${message.client.settings.emojis.info} | Указанная вами категория не была найдена доступных`);
+
+            const embed = new MessageEmbed()
+            .setTitle(`🔧 | Команды модуля ${category.name}`)
+            .setColor(message.client.settings.colors.main)
+            .setFooter(message.guild.name, message.guild.iconURL())
+            .setTimestamp();
+
+            message.client.commands.filter((cmd) => cmd.category === category.key).map((cmd) => {
+                embed.description += `\`${data.prefix}${cmd.name}\` — ${cmd.description}\n`;
+            });
+
+            return message.reply(embed);
         }
     }
 }
