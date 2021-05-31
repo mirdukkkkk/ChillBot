69c0fc2059fa37875b2a76208797bac1f20ce124
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-31 16:28:41 +0300

    Закоментировал строку с статой

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -8,7 +8,7 @@ class ReadyListener extends ChillBotListener {
 
     async run(client) {
         client.user.setActivity(`на сервер Just Chilling`, { type: 3 });
-        setInterval(() => {
+        /*setInterval(() => {
             client.channels.cache.get(client.settings.panel.channel).messages.cache.get(client.settings.panel.message).edit(
                 new MessageEmbed()
                 .setTitle('🔧 | Статистика бота')
@@ -16,7 +16,7 @@ class ReadyListener extends ChillBotListener {
                 .setFooter('ChillBot', this.client.user.displayAvatarURL({ format: 'webp', size: 2048 }))
                 .setTimestamp()
             );
-        }, client.settings.panel.interval);
+        }, client.settings.panel.interval);*/
         console.log('[+] ChillBot был запущен!');
     }
 }
