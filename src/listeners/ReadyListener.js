17176b8a2153bcf3f7c78236f8616515fd0ba64a
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-05-31 20:14:06 +0300

    Панель статистики

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -8,15 +8,16 @@ class ReadyListener extends ChillBotListener {
 
     async run(client) {
         client.user.setActivity(`на сервер Just Chilling`, { type: 3 });
-        /*setInterval(() => {
+        setInterval(() => {
             client.channels.cache.get(client.settings.panel.channel).messages.cache.get(client.settings.panel.message).edit(
                 new MessageEmbed()
                 .setTitle('🔧 | Статистика бота')
                 .setColor(client.settings.colors.main)
+                .setDescription(`⏱️ | Пинг: \`${client.ws.ping}\`\n🎛️ | Использование памяти: \`${(process.memoryUsage().heapUsed / (1000 * 1000)).toFixed(2)} МБ\``)
                 .setFooter('ChillBot', this.client.user.displayAvatarURL({ format: 'webp', size: 2048 }))
                 .setTimestamp()
             );
-        }, client.settings.panel.interval);*/
+        }, client.settings.panel.interval);
         console.log('[+] ChillBot был запущен!');
     }
 }
