c03254b0f2d5cf690823bd438fc7408be1bf7580
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-06-20 18:43:05 +0300

    Добавил команду для выдачи значков

---
+++ src/commands/Special/BadgesCommand.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+const ChillBotCommand = require('../../structures/ChillBotCommand');
+
+class BadgesCommand extends ChillBotCommand {
+    constructor() {
+        super('badges', {
+            description: 'Выдаёт значок определённой цели',
+            category: 'special',
+            usage: '<ID/Упоминание> <ID значка который выходите выдать/отнять>',
+            args: true,
+            public: false
+        });
+    }
+
+    async run(message, args) {
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!member) return message.fail(`${message.client.settings.emojis.warning} | Указанный вами пользователь не был найден!`);
+        let data = await message.client.database.collection('users').findOne({ userID: member.id });
+        if(!data) return message.fail(`${message.client.settings.emojis.warning} | Пользователь отсуствует в базе данных!\n${message.client.settings.emojis.info} | Свяжитесь с создателем бота для дополнительной информации.`);
+        message.client.database.collection('users').updateOne({ userID: member.id }, {
+            [data.badges.includes(args[1]) ? '$pull' : '$push']: {
+                badges: args[1]
+            }
+        });
+        message.react(message.client.settings.emojis.done);
+    }
+}
+
+module.exports = BadgesCommand;
