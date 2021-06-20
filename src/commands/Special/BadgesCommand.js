ca50944155d3f0d7c6878fe1ed1cce7aa780d4d7
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-06-20 19:39:01 +0300

    Фикс

--- src/commands/Special/BadgesCommand.js
+++ src/commands/Special/BadgesCommand.js
@@ -17,7 +17,7 @@ class BadgesCommand extends ChillBotCommand {
         if(!member) return message.fail(`${message.client.settings.emojis.warning} | Указанный вами пользователь не был найден!`);
         let data = await message.client.database.collection('users').findOne({ userID: member.id });
         if(!data) return message.fail(`${message.client.settings.emojis.warning} | Пользователь отсуствует в базе данных!\n${message.client.settings.emojis.info} | Свяжитесь с создателем бота для дополнительной информации.`);
-        if(!message.client.constants.badges.includes(args[1])) return message.fail(`${message.client.settings.emojis.warning} | Указанного вами значка не существует!`)
+        if(!message.client.constants.badgesArray.includes(args[1])) return message.fail(`${message.client.settings.emojis.warning} | Указанного вами значка не существует!`)
         try {
             message.client.database.collection('users').updateOne({ userID: member.id }, {
                 [data.badges.includes(args[1]) ? '$pull' : '$push']: {
