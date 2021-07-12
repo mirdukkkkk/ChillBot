b0ff5d46b097b81b3adedc22e787c1e2bcea3334
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-07-12 20:42:08 +0300

    Улучшил эвал

--- src/commands/Special/EvalCommand.js
+++ src/commands/Special/EvalCommand.js
@@ -1,10 +1,11 @@
+const Discord = require('discord.js');
 const ChillBotCommand = require('../../structures/ChillBotCommand');
 
 class EvalCommand extends ChillBotCommand {
     constructor() {
         super('eval', {
             args: true,
-            aliases: ['e'],
+            aliases: ['e', '>'],
             category: 'special',
             usage: '<код>',
             public: false,
@@ -14,11 +15,33 @@ class EvalCommand extends ChillBotCommand {
 
     async run(message, args) {
         if(!message.client.constants.special_access.includes(message.author.id)) return message.react('❌');
+        const util = require('util');
+        let code = args.join(' ');
+        let isAsync = false;
+
         try {
-            let executed = eval(args.join(' '));
-            message.reply(require('util').inspect(executed, { depth: 0, maxArrayLength: null }), { code: 'js' });
+            if(!code) return message.reply('Введите код, который необходимо выполнить!');
+            code = code.replace(/(```(.+)?)?/g, '');
+
+            if(code.includes('await')) isAsync = true;
+            if(isAsync) code = `(async () => {${code}})()`;
+            const before = process.hrtime.bigint();
+            let executed = eval(code);
+            if(util.types.isPromise(executed)) executed = await executed;
+            const after = process.hrtime.bigint();
+            if(typeof executed !== 'string') executed = util.inspect(executed, { depth: 0, maxArrayLength: null });
+            if(executed.length >= 1940) {
+                message.channel.send('Результат оказался слишком большим, поэтому я отправил его тебе в личку.');
+                return message.member.send(executed, { split: '\n', code: 'js' });
+            }
+            message.reply(executed, { code: 'js' });
         } catch(error) {
             message.reply(error, { code: 'js' });
+        }
+        function clean(text) {
+            return text
+              .replace(/`/g, `\`${String.fromCharCode(8203)}`)
+              .replace(/@/g, `@${String.fromCharCode(8203)}`);
         }
     }
 }
