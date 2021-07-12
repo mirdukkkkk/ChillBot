0353ae9a1f71bb7bdb9afe635428c5f6c44a84f4
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-07-12 20:43:42 +0300

    Логгирование

--- src/masters/CommandsExecutorMaster.js
+++ src/masters/CommandsExecutorMaster.js
@@ -31,14 +31,16 @@ class CommandsExecutorMaster {
         if(command && cmd !== '') {
             if(!this.message.guild.me.permissionsIn(this.message.channel).has('SEND_MESSAGES')) return;
             if(!this.message.guild.me.permissionsIn(this.message.channel).has('EMBED_LINKS')) return this.message.reply(`У меня нет права отпрвлять встроенные сообщения! Предоставте мне это право что-бы я смог работать корректно!`);
-            if(command.premium && this.message.member.roles.cache.has(data.premium)) return premiumRequired(this.message);
+            if(command.premium && this.message.member.roles.cache.has(data.premium) || !this.message.client.constants.special_access.includes(this.message.author.id)) return premiumRequired(this.message);
             if(command.userPermissions.length > 0 && command.userPermissions.some((permission) => !this.message.member.permissions.has(permission))) return this.message.fail(`${this.client.settings.emojis.warning} | У вас недостаточно прав!\n${this.client.settings.emojis.info} | Необходимые права: ${command.userPermissions.map((r) => `\`${permissions[r]}\``).join(', ')}`);
             if(command.args && !args.length) return this.message.fail(`${this.client.settings.emojis.warning} | Недостаточно аргументов!\n${this.client.settings.emojis.info} | Правильное использование команды: \`${data.prefix}${cmd} ${command.usage}\``);
 
             try {
                 command.run(this.message, args);
+                this.client.constants.webhooks.logging.send(this.client.constants.embeds.commandY(cmd, this.message.author));
             } catch(error) {
                 console.error(error);
+                this.client.constants.webhooks.logging.send(this.client.constants.embeds.commandN(cmd, this.message.author, error));
             }
 
             cooldown.set(this.message.author.id, command.name);
