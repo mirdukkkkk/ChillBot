8afdb4ad4b3d223f34acc12d9dbec738dfc8bf67
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-07-26 10:26:22 +0300

    Стайл

--- src/listeners/interactions/InteractionCreateListener.js
+++ src/listeners/interactions/InteractionCreateListener.js
@@ -11,12 +11,13 @@ class InteractionCreateListener extends ChillBotListener {
 
     run(client, interaction) {
         switch(interaction.type) {
-            case 3: 
+            case 3: {
                 if (client.cache.buttons.has(interaction.message.id)) {
                     client.cache.buttons.get(interaction.message.id)(new ChillBotInteractionData(client, interaction));
                     return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 6 }});
                 }
                 break;
+            }
         }
     }
 }
