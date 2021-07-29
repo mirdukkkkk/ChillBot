1dc4833a301fd761581dbc52ec7cca5429fc0caa
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-07-30 00:18:21 +0300

    fix

--- src/commands/Fun/ChessCommand.js
+++ src/commands/Fun/ChessCommand.js
@@ -12,13 +12,11 @@ class ChessCommand extends ChillBotCommand {
 
     async run(message, args) {
         if(message.channel.id == '738534600174862389') return message.reply('Нельзя использовать данную команду в данном канале!');
-        try {
-            message.author.send('https://forms.gle/bzTjPDLXanxTNTWKA <== форма').then(() => {
-                message.reply('Я отправил ссылку в ЛС, заполните форму для участия, потом c вами свяжутся.');
-            });
-        } catch {
-            message.reply('Откройте личные сообщения, для того что-бы я прислал вам ссылку на форму!')
-        }
+        message.author.send('https://forms.gle/bzTjPDLXanxTNTWKA <== форма').then(() => {
+            message.reply('Я отправил ссылку в ЛС, заполните форму для участия, потом c вами свяжутся.');
+        }).catch(() => {
+            return message.reply('Так как я не могу отправить форму вам в ЛС, держите просто ссылку => https://forms.gle/bzTjPDLXanxTNTWKA');
+        })
     }
 }
 
