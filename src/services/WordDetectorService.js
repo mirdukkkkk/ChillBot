e4362d1f48003e6c4b4da7e9e17b3d19de755ae2
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-20 16:41:42 +0300

    WordDetectorService ✅

---
+++ src/services/WordDetectorService.js
@@ -0,0 +1,22 @@
+const WordDetectorData = require('../utils/WordDetectorData');
+
+class WordDetectorService {
+    constructor() {
+        throw new TypeError(`Класс ${this.constructor.name} не может быть инициализирован!`);
+    }
+
+    static async msgReact(message) {
+        if(message.channel.id === "738534600174862389") return;
+        if(!await message.client.database.collection('users').findOne({ id: message.author.id }).reactions) return;
+        const args = message.content.trim().toLowerCase().split(/ +/g);
+        WordDetectorData.msgReact.forEach((obj) => {
+            obj.words.map(async(word) => {
+                if(args.includes(word)) try {
+                    await message.react(obj.emoji);
+                } catch {}
+            });
+        });
+    }
+}
+
+module.exports = WordDetectorService;
