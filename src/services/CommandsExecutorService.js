a8f6033fb358d1f50b8204f71fd4c9e0284c0bfe
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-26 06:28:57 +0300

    CommandsExecutorService ✅ (fixed)

--- src/services/CommandsExecutorService.js
+++ src/services/CommandsExecutorService.js
@@ -19,12 +19,11 @@ class CommandsExecutorService {
     async runCommand() {
         if(this.message.author.bot) return;
         const data = await this.client.database.collection('main').findOne({ name: 'guild' });
-        if(!this.message.content.startsWith(/*data.prefix*/'.')) return;
+        if(!this.message.content.startsWith(data.prefix)) return;
         const [cmd, ...args] = this.message.content.slice(1).trim().split(/ +/g);
         const command = await this.findCommand(cmd);
         if(cooldown.has(this.message.author.id) && cooldown.get(this.message.author.id) === command.name) return this.message.react('⏱️').catch();
         if(command) {
-            if(!command.public && !this.client.constants.special_access.includes(this.message.author.id)) return message.react('❌');
             if(!this.message.guild.me.permissionsIn(this.message.channel).has(Permissions.FLAGS.SEND_MESSAGES)) return;
             if(!this.message.guild.me.permissionsIn(this.message.channel).has(Permissions.FLAGS.EMBED_LINKS)) return this.message.reply(`У меня нет права отпрвлять встроенные сообщения! Предоставте мне это право что-бы я смог работать корректно!`);
             if(command.userPerms.length > 0 && command.userPerms.some((permission) => !this.message.member.permissions.has(permission))) return;
