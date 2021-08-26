b286aaa4acc40b3fcbf309a7508226b2876e0633
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-26 06:21:33 +0300

    Ballcommand ✅

---
+++ src/commands/fun/BallCommand.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+const ChillBotCommand = require('../../structures/ChillBotCommand');
+
+class BallCommand extends ChillBotCommand {
+    constructor() {
+        super('ball', {
+            description: 'Отвечает на заданный вопрос',
+            category: 'fun',
+            usage: '<вопрос>',
+            cooldown: 3
+        });
+    }
+
+    async run(message, args) {
+        if(!args.join(' ')) return message.client.embconstructor.fail(`${message.client.constants.emojis.info} | Введите ваш вопрос!`, message);
+        if(args.join(' ').length > 1024) return message.client.embconstructor.fail(`${message.client.constants.emojis.info} | Длинна вопроса не должна быть длинее 1024 символа!`, message);
+        
+        const answers = message.client.constants.answersBall;
+
+        return message.reply(
+            {
+                embeds: [
+                    new MessageEmbed()
+                    .setTitle('🎱 | 8ball')
+                    .addFields(
+                        { name: 'Вопрос', value: args.join(' '), inline: true },
+                        { name: 'Ответ', value: answers[Math.floor(Math.random() * answers.length)], inline: true }
+                    )
+                    .setColor(message.client.constants.colors.main)
+                    .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                    .setTimestamp()
+                ]
+            }
+        );
+    }
+}
+
+module.exports = BallCommand;
