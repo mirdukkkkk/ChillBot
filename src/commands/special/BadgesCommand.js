6fb2215d58bc20b96899b9cbc43494c980b77fad
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-26 06:24:09 +0300

    BadgesCommand ✅

---
+++ src/commands/special/BadgesCommand.js
@@ -0,0 +1,39 @@
+const ChillBotCommand = require('../../structures/ChillBotCommand');
+const { MessageEmbed } = require(`discord.js`);
+
+class BadgesCommand extends ChillBotCommand {
+    constructor() {
+        super('badges', {
+            description: 'Выдаёт значок определённой цели',
+            category: 'special',
+            usage: '<ID/Упоминание> <ID значка который выходите выдать/отнять>',
+            args: true,
+            public: false,
+            cooldown: 3
+        });
+    }
+
+    async run(message, args) {
+        if(!message.client.constants.special_access.includes(message.author.id)) return message.react('❌');
+        if(args.length === 0) return message.client.embconstructor.fail(`${message.client.constants.emojis.warning} | Пожалуйста, укажите участника, которому вы хотите выдать значок.`, message);
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if(!member) return message.client.embconstructor.fail(`${message.client.constants.emojis.warning} | Указанный вами пользователь не был найден!`, message);
+        if(args.length === 1) return message.client.embconstructor.fail(`${message.client.constants.emojis.warning} | Укажите значок, который вы хотите выдать участнику!`, message);
+        let data = await message.client.database.collection('users').findOne({ id: member.id });
+        if(!data) return message.client.embconstructor.fail(`${message.client.constants.emojis.warning} | Пользователь отсуствует в базе данных!\n${message.client.constants.emojis.info} | Свяжитесь с создателем бота для дополнительной информации.`, message);
+        if(!message.client.constants.badgesArray.includes(args[1])) return message.client.embconstructor.fail(`${message.client.constants.emojis.warning} | Указанного вами значка не существует!`, message);
+
+        try {
+            message.client.database.collection('users').updateOne({ id: member.id }, {
+                [data.badges.includes(args[1]) ? '$pull' : '$push']: {
+                    badges: args[1]
+                }
+            });
+            message.react(message.client.constants.emojis.done);
+        } catch(error) {
+            message.reply(`Произошла неизвестная ошибка. Свяжитесь с разработчиком для дополнительной информации!\n${error}`);
+        }
+    }
+}
+
+module.exports = BadgesCommand;
