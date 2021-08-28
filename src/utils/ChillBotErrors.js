770a280eb3701838182163209daa87fb433f105d
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-28 18:17:01 +0300

    ChillBotErrors (custom)

---
+++ src/utils/ChillBotErrors.js
@@ -0,0 +1,32 @@
+module.exports = {
+    failMessage: [
+        {
+            content: 'Ашибка403',
+            author: 'Морковка'
+        },
+        {
+            content: 'Все поцаны, свёртываемся',
+            author: 'Морковка'
+        },
+        {
+            content: 'вфзыовзф3809у834',
+            author: 'Степашка'
+        },
+        {
+            content: 'Вы можете написать свою ошибку, написав её ко мне в ЛС!',
+            author: 'Морковка'
+        },
+        {
+            content: 'Нельзя',
+            author: 'Степашка'
+        },
+        {
+            content: 'alkogolnayaprincessatop',
+            author: 'Степашка'
+        },
+        {
+            contant: 'кексы с повидлом в шоколадном соусе',
+            author: 'Степашка'
+        }
+    ]
+}
