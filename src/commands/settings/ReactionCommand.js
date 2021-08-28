c515789f8af944809d6f5e95f6ef771852654edc
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-28 17:59:01 +0300

    ReactionCommand +

---
+++ src/commands/settings/ReactionCommand.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js');
+const ChillBotCommand = require('../../structures/ChillBotCommand');
+
+class ReactionCommand extends ChillBotCommand {
+    constructor() {
+        super('reaction', {
+            description: 'Команда для упраления системой реакций',
+            category: 'settings',
+            aliases: ['react', 'r'],
+            cooldown: 15
+        });
+    }
+
+    async run(message, args) {
+        const data = await message.client.database.collection('users').findOne({ id: message.author.id });
+        message.reply({
+            embeds: [
+                new MessageEmbed()
+                .setTitle(`${message.client.constants.emojis.done} | Успешно`)
+                .setDescription(`🍪 | Теперь под вашими сообщениями реакции ставится ${data.reactions ?  '**не будут**' : '**будут**'}`)
+                .setColor(message.client.constants.colors.main)
+                .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                .setTimestamp()
+            ]
+        });
+        return message.client.database.collection('users').updateOne({ id: message.author.id }, {
+            $set: {
+                reactions: data.reactions ? false : true
+            }
+        });
+    }
+}
+
+module.exports = ReactionCommand;
