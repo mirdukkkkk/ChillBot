563475d5349e522b2c46035f4cad0c06e53d990f
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-08-28 18:04:02 +0300

    WordDetectorService fix

--- src/services/WordDetectorService.js
+++ src/services/WordDetectorService.js
@@ -7,7 +7,8 @@ class WordDetectorService {
 
     static async msgReact(message) {
         if(message.channel.id === "738534600174862389") return;
-        if(!await message.client.database.collection('users').findOne({ id: message.author.id }).reactions) return;
+        const data = await message.client.database.collection('users').findOne({ id: message.author.id });
+        if(!data.reactions) return;
         const args = message.content.trim().toLowerCase().split(/ +/g);
         WordDetectorData.msgReact.forEach((obj) => {
             obj.words.map(async(word) => {
