93498546701942d303b332aa3582a04dd04a0feb
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-10-09 11:46:05 +0300

    client partials

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const { Intents } = require('discord.js');
 console.log('[+] ChillBot запускается!');
 
 const client = new ChillBotClient({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-    allowedMentions: { parse: ['users', 'roles'], repliedUser: false }
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS],
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
+    partials: ['CHANNEL', 'MESSAGE']
 });
 
 client._start().catch(console.error);
