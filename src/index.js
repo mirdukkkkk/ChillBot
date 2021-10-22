6c6053e021a3d5fe8d9e902f18460f9ae2e0d37b
haha-classic <69540930+haha-classic@users.noreply.github.com>
2021-10-22 20:01:40 +0300

    add new intent

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Intents } = require('discord.js');
 console.log('[+] ChillBot запускается!');
 
 const client = new ChillBotClient({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.DIRECT_MESSAGES],
     allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
     partials: ['CHANNEL', 'MESSAGE']
 });
