8332106dfb54f9cd5ed86be4e711829cb2d6b7f2
haha-classic <karmulav@gmail.com>
2022-07-23 18:08:07 +0300

    modified:   src/listeners/ReadyListener.js 	modified:   src/services/CommandsExecutorService.js 	modified:   src/utils/ChillBotFunctions.js

--- src/utils/ChillBotFunctions.js
+++ src/utils/ChillBotFunctions.js
@@ -1,13 +1,13 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     dm: (message, client) => {
         if(!message.guild && !message.author.bot) {
-            if(!!message.attachments.first()) {
+            if(message.attachments.first()) {
                 const arr = [];
             message.attachments.map(att => arr.push(att.url));
-                return client.constants.webhooks.dm.send({ embeds: [new MessageEmbed().setTitle('Новое сообщение').setDescription(message.content.length === 0 ? 'Пустое содержание' : message.content).addField(`Вложения`, arr.join('\n'), false).setThumbnail(message.author.displayAvatarURL({ format: 'webp', dynamic: true })).setFooter(`${message.author.tag} | ${message.author.id}`).setTimestamp()] });
-            } else return client.constants.webhooks.dm.send({ embeds: [new MessageEmbed().setTitle('Новое сообщение').setDescription(message.content.length === 0 ? 'Пустое содержание' : message.content).setThumbnail(message.author.displayAvatarURL({ format: 'webp', dynamic: true })).setFooter(`${message.author.tag} | ${message.author.id}`).setTimestamp()] });
+                return client.constants.webhooks.dm.send({ embeds: [new EmbedBuilder().setTitle('Новое сообщение').setDescription(message.content.length === 0 ? 'Пустое содержание' : message.content).addField(`Вложения`, arr.join('\n'), false).setThumbnail(message.author.displayAvatarURL({ format: 'webp', dynamic: true })).setFooter({ text: `${message.author.tag} | ${message.author.id}` }).setTimestamp()] });
+            } else return client.constants.webhooks.dm.send({ embeds: [new EmbedBuilder().setTitle('Новое сообщение').setDescription(message.content.length === 0 ? 'Пустое содержание' : message.content).setThumbnail(message.author.displayAvatarURL({ format: 'webp', dynamic: true })).setFooter({ text: `${message.author.tag} | ${message.author.id}`}).setTimestamp()] });
         }
     },
     
