8b05c5aedc8acafe8196ff8a64a558b8b9485945
haha-classic <karmulav@gmail.com>
2022-08-04 23:48:39 +0700

    database helper update

--- src/helpers/DatabaseHelper.js
+++ src/helpers/DatabaseHelper.js
@@ -3,9 +3,9 @@ class DatabaseHelper {
         throw new TypeError(`Класс ${this.constructor.name} не может быть инициализирован!`);
     }
 
-    static createUserEntry(client, { user, schema, options = {} }) {
+    static async createUserEntry(client, { user, schema, options = {} }) {
         if(!client || !client?.database) throw new TypeError('Необходимо указать действительный экземпляр клиента.');
-        client.database.collection('users').updateOne({ id: user }, {
+        return await client.database.collection('users').updateOne({ id: user }, {
             $set: schema
         }, options);
     }
